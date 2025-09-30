98ee836b7165457d9f57e6b9f3aee3c8295aaff4
Egor Sheff <egor.shevtsov@aisa.ru>
2025-09-30 12:31:07 +0300

    finish

--- aiplan.go/internal/aiplan/business/replace_user.go
+++ aiplan.go/internal/aiplan/business/replace_user.go
@@ -1,13 +1,35 @@
 package business
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
+var (
+	userFKs []userFK
+)
+
+type userFK struct {
+	Table string
+	Field string
+}
+
 func (b *Business) ReplaceUser(origUserId string, newUserId string) error {
 	return b.db.Transaction(func(tx *gorm.DB) error {
-		for _, expr := range userChangeExprs {
-			if err := tx.Exec(expr, newUserId, origUserId).Error; err != nil {
+		for _, fk := range userFKs {
+			tx.SavePoint("preUpdate")
+			if err := tx.Table(fk.Table).
+				Where(fk.Field+"=?", origUserId).
+				Update(fk.Field, newUserId).Error; err != nil {
+				if err == gorm.ErrDuplicatedKey {
+					tx.RollbackTo("preUpdate")
+				} else {
+					return err
+				}
+			}
+
+			if err := tx.Exec(fmt.Sprintf("delete from %s where %s=?", fk.Table, fk.Field), origUserId).Error; err != nil {
 				return err
 			}
 		}
@@ -15,54 +37,18 @@ func (b *Business) ReplaceUser(origUserId string, newUserId string) error {
 	})
 }
 
-// UPDATE THIS LIST IF ADD NEW TABLE WITH FK TO USER ID
-var (
-	userChangeExprs = []string{
-		"update docs set created_by_id=? where created_by_id=?",
-		"update doc_comments set actor_id=? where actor_id=?",
-		"update projects set project_lead_id=? where project_lead_id=?",
-		"update issues set created_by_id=? where created_by_id=?",
-		"update issue_comments set actor_id=? where actor_id=?",
-		"update file_assets set created_by_id=? where created_by_id=?",
-		"update workspaces set owner_id=? where owner_id=?",
-		"update users set created_by_id=? where created_by_id=?",
-		"update users set updated_by_id=? where updated_by_id=?",
-		"update comment_reactions set user_id=? where user_id=?",
-		"update deferred_notifications set user_id=? where user_id=?",
-		"update doc_activities set actor_id=? where actor_id=?",
-		"update doc_comment_reactions set user_id=? where user_id=?",
-		"update doc_editors set editor_id=? where editor_id=?",
-		"update doc_readers set reader_id=? where reader_id=?",
-		"update doc_watchers set watcher_id=? where watcher_id=?",
-		"update forms set created_by_id=? where created_by_id=?",
-		"update entity_activities set actor_id=? where actor_id=?",
-		"update form_activities set actor_id=? where actor_id=?",
-		"update form_answers set created_by_id=? where created_by_id=?",
-		"update issue_activities set actor_id=? where actor_id=?",
-		"update issue_assignees set assignee_id=? where assignee_id=?",
-		"update issue_links set created_by_id=? where created_by_id=?",
-		"update issue_properties set user_id=? where user_id=?",
-		"update issue_templates set created_by_id=? where created_by_id=?",
-		"update issue_templates set updated_by_id=? where updated_by_id=?",
-		"update issue_watchers set watcher_id=? where watcher_id=?",
-		"update project_activities set actor_id=? where actor_id=?",
-		"update project_members set member_id=? where member_id=?",
-		"update project_members set created_by_id=? where created_by_id=?",
-		"update release_notes set author_id=? where author_id=?",
-		"update activities set actor_id=? where actor_id=?",
-		"update rules_log set user_id=? where user_id=?",
-		"update search_filters set author_id=? where author_id=?",
-		"update user_search_filters set user_id=? where user_id=?",
-		"update sessions_resets set user_id=? where user_id=?",
-		"update tariffication set user_id=? where user_id=?",
-		"update team_members set member_id=? where member_id=?",
-		"update user_feedbacks set user_id=? where user_id=?",
-		"update workspace_activities set actor_id=? where actor_id=?",
-		"update user_notifications set user_id=? where user_id=?",
-		"update user_notifications set author_id=? where author_id=?",
-		"update workspace_backups set created_by=? where created_by=?",
-		"update workspace_members set member_id=? where member_id=?",
-		"update workspace_members set created_by_id=? where created_by_id=?",
-		"update issue_description_locks set user_id=? where user_id=?",
-	}
-)
+func (b *Business) PopulateUserFKs() error {
+	return b.db.Raw(`SELECT
+    tc.table_name as Table,
+    kcu.column_name as Field
+FROM
+    information_schema.table_constraints AS tc
+    JOIN information_schema.key_column_usage AS kcu
+        ON tc.constraint_name = kcu.constraint_name
+    JOIN information_schema.constraint_column_usage AS ccu
+        ON ccu.constraint_name = tc.constraint_name
+WHERE
+    tc.constraint_type = 'FOREIGN KEY'
+    AND ccu.table_name = 'users'
+    AND ccu.column_name = 'id'`).Find(&userFKs).Error
+}
--- aiplan.go/internal/aiplan/business/replace_user_test.go
+++ aiplan.go/internal/aiplan/business/replace_user_test.go
@@ -1,6 +1,7 @@
 package business
 
 import (
+	"fmt"
 	"testing"
 
 	"gorm.io/driver/postgres"
@@ -9,21 +10,39 @@ import (
 	"sheff.online/aiplan/internal/aiplan/dao"
 )
 
+func TestPopulateFKs(t *testing.T) {
+	cfg := config.ReadConfig()
+
+	db, _ := gorm.Open(postgres.New(postgres.Config{
+		DSN: cfg.DatabaseDSN,
+	}), &gorm.Config{TranslateError: true})
+	b := Business{db: db}
+
+	if err := b.PopulateUserFKs(); err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(userFKs)
+}
+
 func TestReplace(t *testing.T) {
 	cfg := config.ReadConfig()
 
 	db, _ := gorm.Open(postgres.New(postgres.Config{
 		DSN: cfg.DatabaseDSN,
 	}), &gorm.Config{TranslateError: true})
-	db = db.Debug()
+	//db = db.Debug()
 
 	b := Business{db: db}
 
-	if err := b.ReplaceUser("b004906a-009a-4ab0-b06c-fe3979f75295", "f2930ffe-a328-4164-a7ff-3e5de4c77755"); err != nil {
+	if err := b.PopulateUserFKs(); err != nil {
 		t.Fatal(err)
 	}
 
-	if err := db.Unscoped().Where("id = ?", "b004906a-009a-4ab0-b06c-fe3979f75295").Delete(&dao.User{}).Error; err != nil {
+	if err := b.ReplaceUser("bb3828d2-1aa6-451d-9666-43a9f7aa0939", "44361aa5-b325-48bf-8c10-e9477615d219"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Unscoped().Where("id = ?", "bb3828d2-1aa6-451d-9666-43a9f7aa0939").Delete(&dao.User{}).Error; err != nil {
 		t.Fatal(err)
 	}
 }
