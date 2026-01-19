626b86b2e822ac51da88a8170cbe7f242f64275d
koposov_igor <koposov.igor@aisa.ru>
2026-01-19 16:07:44 +0700

    fix tgIssue

--- aiplan.go/internal/aiplan/notifications/email-service.go
+++ aiplan.go/internal/aiplan/notifications/email-service.go
@@ -480,7 +480,7 @@ func (es *EmailService) MessageNotify(notification dao.DeferredNotifications, ms
 }
 
 func (es *EmailService) DeadlineMessageNotify(user dao.User, notification dao.DeferredNotifications, nd notifyDeadline) error {
-	subject := fmt.Sprintf("Уведомление об истечении срока выполнения задачи: %s-%d", notification.Project.Identifier, notification.Issue.SequenceId)
+	subject := fmt.Sprintf("Уведомление об истечении срока выполнения задачи: %s", notification.Issue.FullIssueName())
 
 	loc := time.Location(user.UserTimezone)
 	date := nd.Deadline.In(&loc)
