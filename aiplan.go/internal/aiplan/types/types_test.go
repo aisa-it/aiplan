cd2f541fc317ac912c3e875de3cf64868e5e5355
Egor Sheff <egor.shevtsov@aisa.ru>
2026-02-11 18:21:26 +0300

    fix JSONTime

--- aiplan.go/internal/aiplan/types/types_test.go
+++ aiplan.go/internal/aiplan/types/types_test.go
@@ -4,6 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/aisa-it/aiplan/aiplan.go/internal/aiplan/config"
+	"github.com/stretchr/testify/require"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func TestUUID(t *testing.T) {
@@ -15,4 +21,28 @@ func TestUUID(t *testing.T) {
 	d, err := json.Marshal(ss)
 	fmt.Println(err)
 	fmt.Println(string(d))
+}
+
+func TestJSONTime(t *testing.T) {
+	tt := IssuesListFilters{
+		CreatedAtFrom: JSONTime(time.Now()),
+	}
+
+	d, err := json.Marshal(tt)
+	require.NoError(t, err)
+	fmt.Println(string(d))
+
+	var ttUn IssuesListFilters
+	require.NoError(t, json.Unmarshal(d, &ttUn))
+	fmt.Println(ttUn.CreatedAtFrom.Time())
+}
+
+func TestJSONTimeDB(t *testing.T) {
+	cfg := config.ReadConfig()
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: cfg.DatabaseDSN}))
+	require.NoError(t, err)
+
+	var tt bool
+	fmt.Println(db.Raw("select $1 > NOW()", JSONTime(time.Now())).Scan(&tt).Error)
+	fmt.Println(tt)
 }
